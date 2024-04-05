677b88630b312af7ba8a0d1a886bee08a97e85aa
Dillion Verma <hello@dillion.io>
2024-04-04 20:22:11 -0400

    fix: remove dependency on sig

--- audiocraft/utils/checkpoint.py
+++ audiocraft/utils/checkpoint.py
@@ -53,27 +53,19 @@ def is_sharded_checkpoint(path: Path) -> bool:
     return re.search(r'\.th\.\d+$', path.name) is not None
 
 
-def resolve_checkpoint_path(sig_or_path: tp.Union[Path, str], name: tp.Optional[str] = None,
+def resolve_checkpoint_path(path: tp.Union[Path, str], name: tp.Optional[str] = None,
                             use_fsdp: bool = False) -> tp.Optional[Path]:
-    """Resolve a given checkpoint path for a provided dora sig or path.
+    """Resolve a given checkpoint path.
 
     Args:
-        sig_or_path (Path or str): Checkpoint path or dora signature.
+        path (Path or str): Checkpoint path.
         name (str, optional): Name suffix for the checkpoint file stem.
-        rank (optional, int): Rank for distributed processing, retrieved with flashy if not provided.
         use_fsdp (bool): Whether the calling solver relies on FSDP.
     Returns:
         Path, optional: Resolved checkpoint path, if it exists.
     """
-    from audiocraft import train
-    xps_root = train.main.dora.dir / 'xps'
-    sig_or_path = str(sig_or_path)
-    if sig_or_path.startswith('//sig/'):
-        sig = sig_or_path[len('//sig/'):]
-        path = xps_root / sig
-    else:
-        path = Path(sig_or_path)
-        path = AudioCraftEnvironment.resolve_reference_path(path)
+    path = Path(path)
+    path = AudioCraftEnvironment.resolve_reference_path(path)
 
     if path.is_dir():
         path = path / checkpoint_name(name, use_fsdp=use_fsdp)
